18a970f13fd1a9110b2e5c4f638766248f45507a
elizabethvincentm <elizabethvincentm@gmail.com>
2020-12-08 15:44:20 +0530

    feat:initial commit - create react app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p class="bg-blue-100">
+          Edit <code> src / App.js </code> and save to reload.{" "}
         </p>
         <a
           className="App-link"
@@ -15,8 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
+          Learn React{" "}
+        </a>{" "}
       </header>
     </div>
   );
